0815080747e902a39682cc3ca4dc6b2c40a44283
congnghiahieu <21020540@vnu.edu.vn>
2022-10-03 18:24:31 +0700

    Remove public/css folder

---
+++ src/main.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const morgan = require('morgan');
+const express = require('express');
+const {engine} = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+// Use static files
+// __dirname chỉ tới: path_to_project/src
+app.use(express.static(path.join(__dirname,'public')));
+
+// HTTP logger
+app.use(morgan('dev'));
+
+// Template engines
+app.engine('hbs',engine(
+  // Config options
+  {
+    extname: '.hbs',
+  }
+));
+app.set('view engine','hbs');
+app.set('views',path.join(__dirname,'resources/views'));
+
+// Render
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/news', (req, res) => {
+  res.render('news');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
