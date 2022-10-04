6870f7b813eff1d359169e331ef2bb49478ca5ac
congnghiahieu <21020540@vnu.edu.vn>
2022-10-04 19:04:07 +0700

    Install prettier, lint-staged, husky

--- src/main.js
+++ src/main.js
@@ -1,41 +1,44 @@
-const path = require("path");
-const morgan = require("morgan");
-const express = require("express");
-const { engine } = require("express-handlebars");
+const path = require('path');
+const morgan = require('morgan');
+const express = require('express');
+const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-const route = require("./routes/index");
+const route = require('./routes/index');
 
 // Use static files
 // __dirname chỉ tới: path_to_project/src
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    }),
 );
 app.use(express.json());
 
 // HTTP logger
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 // Template engines
 app.engine(
-  "hbs",
-  engine(
-    // Config options
-    {
-      extname: ".hbs",
-    }
-  )
+    'hbs',
+    engine(
+        // Config options
+        {
+            extname: '.hbs',
+        },
+    ),
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+                            app.set('view engine', 'hbs');
+                app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routes init
-route(app);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+
+
+            route(app);
+
+                app.listen(port, () => {
+                    console.log(`Example app listening on port ${port}`);
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
-const newsRouter = require("./news");
-const siteRouter = require("./site");
+const newsRouter = require('./news');
+const siteRouter = require('./site');
 function route(app) {
-  // Summary all routes
-  app.use("/news", newsRouter);
-  app.use("/", siteRouter);
+    // Summary all routes
+    app.use('/news', newsRouter);
+    app.use('/', siteRouter);
 }
 
 module.exports = route;
